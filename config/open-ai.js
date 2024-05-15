e0007fc17ba73419f6f6891f9a2b6c062ff7e547
abd-allh <abdallah.ismail698@gmail.com>
2024-05-15 21:44:24 +0300

    Update

--- config/open-ai.js
+++ config/open-ai.js
@@ -1,11 +1,11 @@
-import { Configuration, OpenAIApi } from 'openai';
-import dotenv from 'dotenv';
-dotenv.config();
+const { Configuration, OpenAIApi } = require("openai")
+const dotenv = require("dotenv")
+dotenv.config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-});
+})
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration)
 
-export default openai;
+exports.openai = openai
